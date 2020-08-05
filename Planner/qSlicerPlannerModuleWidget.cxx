6b134352eda020b07479a1ee2f4a11f2c111c3e5
James Hoctor <james.hoctor@kitware.com>
2020-08-04 22:52:55 -0400

    Revert "COMP: Comment peripheral subsystems"
    This reverts commit f3cd190c64e4532e90e0092e362392834876ccff.

--- Planner/qSlicerPlannerModuleWidget.cxx
+++ Planner/qSlicerPlannerModuleWidget.cxx
@@ -44,7 +44,7 @@
 #include "qSlicerApplication.h"
 #include "qSlicerIOManager.h"
 #include "qSlicerFileDialog.h"
-// #include "qMRMLSubjectHierarchyModel.h"
+#include "qMRMLSceneModelHierarchyModel.h"
 #include "qSlicerPlannerModuleWidget.h"
 #include "ui_qSlicerPlannerModuleWidget.h"
 #include "qMRMLSortFilterProxyModel.h"
@@ -95,7 +95,7 @@
 #include <vtkSlicerCLIModuleLogic.h>
 
 // Self
-// #include "qMRMLPlannerModelHierarchyModel.h"
+#include "qMRMLPlannerModelHierarchyModel.h"
 #include "vtkSlicerPlannerLogic.h"
 #include "ButtonItemDelegate.h"
 
